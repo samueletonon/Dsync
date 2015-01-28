ca3f958d9fd8937460b69023782faf49a32a1a96
samuripper <samu@linuxasylum.net>
2015-01-28 18:16:11 +0100

    trying to avoid array out of bond

--- app/src/main/java/org/samux/samu/dsync/MainActivity.java
+++ app/src/main/java/org/samux/samu/dsync/MainActivity.java
@@ -241,15 +241,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         public void CopyStream(long size, InputStream is, OutputStream os) {
-            final int buffer_size = 1024;
+            final int buffer_size = 4096;
             try {
                 byte[] bytes = new byte[buffer_size];
-                for (int count=0,prog=0;count>=0;) {
-                    count = is.read(bytes, 0, buffer_size);
+                for (int count=0,prog=0;count!=-1;) {
+                    count = is.read(bytes);
                     os.write(bytes, 0, count);
                     prog=prog+count;
                     publishProgress(((long) prog)*100/size);
                 }
+                os.flush();
+                is.close();
+                os.close();
             } catch (Exception ex) {
                 Log.e(TAG,"CS "+ex);
             }
